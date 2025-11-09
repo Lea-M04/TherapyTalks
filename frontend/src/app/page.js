c11243767b07b3e167fce98188c752f68dec7d01
diellav <diellaveliu@gmail.com>
2025-11-09 23:23:19 +0100

    Konfigurimi i next.js

---
+++ frontend/src/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-4xl font-bold">
+      Tailwind v4
+    </main>
+  )
+}
